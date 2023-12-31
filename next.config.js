2c30c18d388d9b3ef24cc7a95b815481ba6de9e3
unknown <collin@stygiansoftworks.com>
2023-12-31 15:50:44 -0500

    newCwars

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['th.bing.com', 'picsum.photos'], // Add the domains you need here
+  },
 }
 
 module.exports = nextConfig
