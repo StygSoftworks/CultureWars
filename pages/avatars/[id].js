19fd8c9241179c2c15d28fcb77068b3f1f76f2e9
unknown <collin@stygiansoftworks.com>
2024-01-07 19:06:04 -0500

    modified:   pages/avatars/[id].js 	add: components/AvatarNavigation.js

--- pages/avatars/[id].js
+++ pages/avatars/[id].js
@@ -1,14 +1,14 @@
 import Layout from '../../components/Layout';
-import React, { useRef, useEffect, useState, useMemo } from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import NextImage from 'next/image';
-
-// Load avatars.json
-const avatarsData = require('../../public/json/avatars.json');
+import avatarsData from '../../public/json/avatars.json';
+import AvatarNavigation from '../../components/AvatarNavigation';
 
 const minId = Math.min(...avatarsData.map((avatar) => avatar.id));
 const maxId = Math.max(...avatarsData.map((avatar) => avatar.id));
+
 
 
 const AvatarDetail = ({ avatarData }) => {
@@ -17,96 +17,28 @@ const AvatarDetail = ({ avatarData }) => {
   }
 
   const handleDownloadClick = async () => {
-    const WIDTH = 750;
-    const HEIGHT = 1050;
-    console.log('avatarData.image', avatarData.image);
-  
-    // Create a canvas
-    const canvas = document.createElement('canvas');
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
-    const ctx = canvas.getContext('2d');
-
-    // Pre-calculate values
-    const centerX = WIDTH / 2;
-    const textWidth = WIDTH - 100; // 100 is 2 * picWidthOffset
-    const picHeight = 300;
-    const picWidthOffset = 50;
-    const picHeightOffset = 50;
-
-    // Set common styles
-    ctx.fillStyle = '#000';
-    ctx.font = 'bold 50px sans-serif';
-    ctx.textAlign = 'center';
-
-    // Load the image
-    const image = new Image();
-    image.crossOrigin = 'Anonymous';
-    image.src = `/images/avatars/${avatarData.id}.webp`;
-
     try {
-        await new Promise((resolve, reject) => {
-            image.onload = resolve;
-            image.onerror = reject;
-        });
-
-        // Draw operations
-        ctx.fillStyle = '#fff';
-        ctx.fillRect(0, 0, WIDTH, HEIGHT);
-        ctx.strokeStyle = '#000';
-        ctx.lineWidth = 20;
-        ctx.strokeRect(0, 0, WIDTH, HEIGHT);
-        ctx.drawImage(image, picWidthOffset, picWidthOffset, WIDTH - (2 * picWidthOffset), picHeight);
-
-        //round the corners
-        ctx.beginPath();
-        ctx.moveTo(20, 0);
-        ctx.lineTo(WIDTH - 20, 0);
-        ctx.quadraticCurveTo(WIDTH, 0, WIDTH, 20);
-        ctx.lineTo(WIDTH, HEIGHT - 20);
-        ctx.quadraticCurveTo(WIDTH, HEIGHT, WIDTH - 20, HEIGHT);
-        ctx.lineTo(20, HEIGHT);
-        ctx.quadraticCurveTo(0, HEIGHT, 0, HEIGHT - 20);
-        ctx.lineTo(0, 20);
-        ctx.quadraticCurveTo(0, 0, 20, 0);
-        ctx.closePath();
-        ctx.clip();
-
-        // Set common styles
-        ctx.fillStyle = '#000';
-        ctx.font = 'bold 50px sans-serif';
-        ctx.textAlign = 'center';
-        const drawText = (text, yOffset) => {
-            ctx.fillText(text, centerX, picHeight + yOffset, textWidth);
-        };
-
-
-        const picStartY = 100 ;
-        drawText(avatarData.name,picStartY+ picHeightOffset);
-        drawText(avatarData.description, picStartY+ (2 * picHeightOffset));
-        drawText(`Power: ${avatarData.power}`,picStartY+  (3 * picHeightOffset));
-        drawText(`Advantage: ${avatarData.advantage}`, picStartY+ (4 * picHeightOffset));
-        drawText(`Disadvantage: ${avatarData.disadvantage}`, picStartY+ (5 * picHeightOffset));
-
-        // Convert the canvas to a data URL
-        const dataURL = canvas.toDataURL('image/png');
-
-        // Creating a link to trigger download
-        const link = document.createElement('a');
-        link.download = `${avatarData.name.replace(/\s/g, '-')}.png`;
-        link.href = dataURL;
-        document.body.appendChild(link); // Append to body to ensure visibility in some browsers
-        link.click();
-        document.body.removeChild(link); // Remove the link after clicking
-
+      const dataURL = await generateAvatarImage(avatarData);
+      triggerImageDownload(`${avatarData.name.replace(/\s/g, '-')}.png`, dataURL);
     } catch (error) {
-        console.error("Error loading image:", error);
+      console.error("Error generating avatar image:", error);
     }
-};
+  };
 
+
+    // Function to find the previous and next avatar names
+    const findAdjacentAvatars = (currentName) => {
+      const sortedAvatars = avatarsData.sort((a, b) => a.name.localeCompare(b.name));
+      const currentIndex = sortedAvatars.findIndex(avatar => avatar.name === currentName);
+      const prevAvatar = sortedAvatars[currentIndex - 1] || sortedAvatars[sortedAvatars.length - 1];
+      const nextAvatar = sortedAvatars[currentIndex + 1] || sortedAvatars[0];
   
+      return { prevAvatar, nextAvatar };
+    };
+  
+    const { prevAvatar, nextAvatar } = findAdjacentAvatars(avatarData.name);
 
-  const { name, description, advantage, disadvantage, image, power, rant, id } = avatarData;
+  const { name, description, advantage, disadvantage, power, rant } = avatarData;
 
   return (
     <Layout>
@@ -114,10 +46,7 @@ const AvatarDetail = ({ avatarData }) => {
 
         <div className="px-6 py-4">
           <NextImage  src={`/images/avatars/${avatarData.id}.webp`} alt={name} width={750} height={1050} />
-
-          {/* <button onClick={handleDownloadClick}>Download</button> */}
         </div>
-
 
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2 text-gray-900">{name}</div>
@@ -138,18 +67,74 @@ const AvatarDetail = ({ avatarData }) => {
           ))}
         </div>
         <div className="flex justify-between px-6 py-4">
-          <Link href={`/avatars/${avatarData.id === minId ? maxId : avatarData.id - 1}`} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            Previous Avatar
-          </Link>
-          <Link href={`/avatars/${avatarData.id === maxId ? minId : avatarData.id + 1}`} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-            Next Avatar
-          </Link>
+
+          <AvatarNavigation prevAvatar={prevAvatar} nextAvatar={nextAvatar} />
+          
         </div>
         
       </div>
     </Layout>
   );
 };
+
+
+// Constants for canvas dimensions and styles
+const CANVAS_WIDTH = 750;
+const CANVAS_HEIGHT = 1050;
+const TEXT_WIDTH = CANVAS_WIDTH - 100;
+const PIC_HEIGHT = 300;
+const PIC_OFFSET = 50;
+
+// Function to generate avatar image
+const generateAvatarImage = async (avatarData) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = CANVAS_WIDTH;
+  canvas.height = CANVAS_HEIGHT;
+  const ctx = canvas.getContext('2d');
+
+  // Common styles
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillStyle = '#000';
+  ctx.font = 'bold 50px sans-serif';
+  ctx.textAlign = 'center';
+
+  // Load the image
+  const image = new Image();
+  image.crossOrigin = 'Anonymous';
+  image.src = `/images/avatars/${avatarData.id}.webp`;
+
+  await new Promise((resolve, reject) => {
+    image.onload = resolve;
+    image.onerror = reject;
+  });
+
+  // Draw the image and texts
+  ctx.drawImage(image, PIC_OFFSET, PIC_OFFSET, CANVAS_WIDTH - (2 * PIC_OFFSET), PIC_HEIGHT);
+  drawText(ctx, avatarData.name, 450);
+  drawText(ctx, avatarData.description, 530);
+  drawText(ctx, `Power: ${avatarData.power}`, 610);
+  drawText(ctx, `Advantage: ${avatarData.advantage}`, 690);
+  drawText(ctx, `Disadvantage: ${avatarData.disadvantage}`, 770);
+
+  return canvas.toDataURL('image/png');
+};
+
+// Helper function to draw text
+const drawText = (ctx, text, yOffset) => {
+  ctx.fillText(text, CANVAS_WIDTH / 2, yOffset, TEXT_WIDTH);
+};
+
+// Function to trigger image download
+const triggerImageDownload = (fileName, dataURL) => {
+  const link = document.createElement('a');
+  link.download = fileName;
+  link.href = dataURL;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
